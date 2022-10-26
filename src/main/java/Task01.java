84ad6809624fa519ccc56fecea22450bc54c53a1
dobri.dobrev@mobilewaves.com <dobri.dobrev@mobilewaves.com>
2022-10-26 14:20:50 +0300

    task 1 big decimal

--- src/main/java/Task01.java
+++ src/main/java/Task01.java
@@ -1,24 +1,23 @@
-import java.math.BigDecimal;
-import java.util.Scanner;
+
 
 public class Task01 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Please enter your first number: ");
-        BigDecimal num1 = new BigDecimal(scan.nextLine());
-        System.out.print("Please enter your second number: ");
-        BigDecimal num2 = new BigDecimal(scan.nextLine());
-        System.out.print("Please enter your third number: ");
-        BigDecimal num3 = new BigDecimal(scan.nextLine());
-
-        if (num1.compareTo(num2) < 0) {
-            if (num3.compareTo(num2) < 0 && num3.compareTo(num1) > 0) {
-                System.out.println(num3 + " is between " + num1 + " and " + num2);
-            }
-        } else {
-            if (num3.compareTo(num1) < 0 && num3.compareTo(num2) > 0) {
-                System.out.println(num3 + " is between " + num2 + " and " + num1);
-            }
-        }
-    }
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Please enter your first number: ");
+//        BigDecimal num1 = new BigDecimal(scan.nextLine());
+//        System.out.print("Please enter your second number: ");
+//        BigDecimal num2 = new BigDecimal(scan.nextLine());
+//        System.out.print("Please enter your third number: ");
+//        BigDecimal num3 = new BigDecimal(scan.nextLine());
+//
+//        if (num1.compareTo(num2) < 0) {
+//            if (num3.compareTo(num2) < 0 && num3.compareTo(num1) > 0) {
+//                System.out.println(num3 + " is between " + num1 + " and " + num2);
+//            }
+//        } else {
+//            if (num3.compareTo(num1) < 0 && num3.compareTo(num2) > 0) {
+//                System.out.println(num3 + " is between " + num2 + " and " + num1);
+//            }
+//        }
+//    }
 }
