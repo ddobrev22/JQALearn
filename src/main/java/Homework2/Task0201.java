e37991bda9a62b20c01652b1e72105bbbbf944af
dobri.dobrev@mobilewaves.com <dobri.dobrev@mobilewaves.com>
2022-11-11 14:13:06 +0200

    task 0201 0202

---
+++ src/main/java/Homework2/Task0201.java
@@ -0,0 +1,18 @@
+package Homework2;
+
+import java.util.Scanner;
+
+public class Task0201 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 1st number:");
+        int num1 = Integer.parseInt(scan.nextLine());
+        System.out.println("Enter 2nd number:");
+        int num2 = Integer.parseInt(scan.nextLine());
+
+        myMethod(num1,num2);
+    }
+    public static void myMethod(int num1, int num2){
+        System.out.printf("%d + %d = %d",num1, num2, num1+num2);
+    }
+}
---
+++ src/main/java/Homework2/Task0202.java
@@ -0,0 +1,21 @@
+package Homework2;
+
+import java.util.Scanner;
+
+public class Task0202 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 1st number:");
+        int num1 = Integer.parseInt(scan.nextLine());
+        System.out.println("Enter 2nd number:");
+        int num2 = Integer.parseInt(scan.nextLine());
+        System.out.println("Average is: " + myMethod(num1,num2));
+
+
+    }
+
+    private static double myMethod(int num1, int num2) {
+
+        return (num1 + num2) /2.0;
+    }
+}
