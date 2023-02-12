1efcb9a3ade16ed0d7ea7804e40e506ee9bc5dfd
dobri.dobrev@mobilewaves.com <dobri.dobrev@mobilewaves.com>
2023-02-12 08:20:04 +0200

    chromedriver.exe

---
+++ src/main/java/hw8/test.java
@@ -0,0 +1,13 @@
+package hw8;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello world ");
+        System.setProperty("webdriver.chrome.driver","C:\\Programs\\chromedriver022023\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+ //       driver.quit();
+    }
+}
