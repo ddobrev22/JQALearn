c1915209cfda7c2f31e75afa3799bd99491e9500
dobri.dobrev@mobilewaves.com <dobri.dobrev@mobilewaves.com>
2023-02-12 09:06:12 +0200

    ? branch 'origin/master'

--- src/main/java/hw8/test.java
+++ src/main/java/hw8/test.java
@@ -1,12 +1,13 @@
 package hw8;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello world ");
         System.setProperty("webdriver.chrome.driver","C:\\Programs\\chromedriver022023\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
  //       driver.quit();
     }
