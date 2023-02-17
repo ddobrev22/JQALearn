ce0f6e31e636e693b4b03f85141cf2a3d61abd72
dobri.dobrev@mobilewaves.com <dobri.dobrev@mobilewaves.com>
2023-02-17 10:28:06 +0200

    hw8

--- src/main/java/Homework8/firstTestsChrome.java
+++ src/main/java/Homework8/firstTestsChrome.java
@@ -1,7 +1,6 @@
 package Homework8;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,20 +15,22 @@ public class firstTestsChrome {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver110.exe");
         ChromeDriver driver=new ChromeDriver();
 
+        driver.manage().window().maximize();
+
         driver.navigate().to("https://www.abv.bg");
-        driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+
 
    //     String next = scan.nextLine();
    //     System.out.println("timeout");
 
         WebDriverWait w = new WebDriverWait(driver, 5);
         w.until(ExpectedConditions.presenceOfElementLocated(
-                By.id("didomi-notice-agree-button")));
+                By.xpath("//iframe[@id='abv-GDPR-frame']")));
 
-        WebElement cookieAgree = driver.findElement(By.id("didomi-notice-agree-button"));
-        cookieAgree.click();
-
-
+        System.out.println("timeout");
+        driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+        driver.wait(1000);
+        driver.findElement(By.xpath("//iframe[@id='abv-GDPR-frame']")).click();
 
     }
 }
--- src/main/java/Homework8/firstTestsFirefox.java
+++ src/main/java/Homework8/firstTestsFirefox.java
@@ -1,10 +1,11 @@
 package Homework8;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class fistTestsFirefox {
+public class firstTestsFirefox {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
 
@@ -13,7 +14,9 @@ public class fistTestsFirefox {
         capabilities.setCapability("marionette",true);
         WebDriver driver= new FirefoxDriver(capabilities);
 
+
+
         // Launch Website
-        driver.navigate().to("http://www.javatpoint.com/");
+   //     driver.navigate().to("http://www.javatpoint.com/");
     }
 }
