63a10bca1e601c318e62f60233b49213e5a45365
dobri.dobrev@mobilewaves.com <dobri.dobrev@mobilewaves.com>
2023-02-20 20:38:06 +0200

    homework 8 with dummi site, user not logged

--- src/main/java/Homework8/firstTestsChrome.java
+++ src/main/java/Homework8/firstTestsChrome.java
@@ -13,21 +13,15 @@ public class firstTestsChrome {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver110.exe");
         WebDriver driver=new ChromeDriver();
 
-
         driver.get("https://parabank.parasoft.com/parabank/index.htm");
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
-     //   driver.findElement(By.cssSelector("#didomi-notice-agree-button")).click();
-
-
         driver.findElement(By.cssSelector("#loginPanel > form > div:nth-child(2) > input")).sendKeys("abc");
 
         driver.findElement(By.cssSelector("#loginPanel > form > div:nth-child(4) > input")).sendKeys("1234");
 
-
-        // Click on the search button
         driver.findElement(By.cssSelector("#loginPanel > form > div:nth-child(5) > input")).click();
 
      //  driver.quit();
